da794b6aa93eeaea7c5b30532dd37f421dbf8be2
osmozoxan-ops <osmozoxan@gmail.com>
2026-01-24 20:13:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+   base: process.env.NODE_ENV === 'production' 
+    ? '/finance-dashboard/' 
+    : '/',    
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
